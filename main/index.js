2370115f71ce45777916c9a51cff916e1148f6e6
Alex <137065037+al3xts@users.noreply.github.com>
2024-12-02 13:40:00 +0100

    first commit

---
+++ main/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Main = ({}) => {
+
+    return (
+        <div className={style}>
+            This is a test
+        </div>
+    );
+};
+
+Main.propTypes = {
+
+};
+
+export default Main;
